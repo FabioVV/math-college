568327883f75d3c998c6a65f5da8a7b08a1465d6
Fabio <fabio-varela@protonmail.com>
2024-05-13 18:00:53 -0300

    More cleaning and some fixing as well

--- python/_Pandas.py
+++ python/_Pandas.py
@@ -1,17 +1,38 @@
 import pandas as pd
 
-dfs = pd.read_excel("./Sales.xlsx", sheet_name="Plan1")
-#display(dfs)
+table = pd.read_excel("./Sales.xlsx", sheet_name="Plan1")
+# (change all of the print statements in this file to a display())
+# When on jupyter notebook
+# (Only work's there i think)
 
-len_table = len(dfs)
+print(table.shape) # número de linhas e colunas
 
-faturamento = dfs['Valor Final'].sum()
+len_table = len(table)
 
-#fpl = dfs[['ID Loja', 'Valor Final']].groupby("ID Loja").sum()
-#display(fpl)
+faturamento = table['Valor Final'].sum()
+print(f"faturamento total = R$ {faturamento:.2f}")
 
-fpp = dfs[['ID Loja', 'Produto', "Valor Final"]].groupby(["ID Loja","Produto"]).sum()
-print(fpp)
+'''
+Faturamento por loja
+'''
+print("Faturamento Por Loja")
+FPL = table[["ID Loja","Valor Final"]]
+table(FPL)
 
-_result = dfs[['ID Loja', 'Produto', "Valor Final"]].groupby(["ID Loja","Produto"]).sum()
-print(_result)
+print("Agrupando por nome da loja (consta como id na tabela (???))")
+FPL = table[["ID Loja","Valor Final"]].groupby("ID Loja").sum()
+print(FPL)
+
+
+print("Faturamento por produto")
+FPP = table[['ID Loja', 'Produto', "Valor Final"]].groupby(["ID Loja","Produto"]).sum()
+print(FPP)
+
+
+'''
+Possivel solução para aumentar o faturamento da empresa.
+A loja Iguatemi Campinas vende uma bermuda denominada "Bermuda Liso" onde
+o faturamento dessa bermuda foi de R$ 36581,00
+As outras lojas não vendem essa bermuda e uma sugestão para aumentar o faturamento
+é que todas as lojas passem a vender a bermuda denominada "Bermuda Liso"
+'''
